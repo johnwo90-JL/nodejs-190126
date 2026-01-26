d560548cd7d434295f3911af125779cbc5eb37b4
johnwo90-JL <john-age@jobloop.no>
2026-01-26 12:19:27 +0100

    WIP

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-import http from "node:http";
+// import http from "node:http";
+
+import express from "express";
+
+const app = express();
 
 const PORT = 3000;
 const HOST = "127.0.0.1"; // "localhost", "0.0.0.0"
 
-const server = http.createServer((req, res) => {
-    console.log("Request – URL:", req.url);
-    console.log("Request – Method:", req.method);
-    console.log("Request – Headers:", req.headers);
-    console.log("Request – HTTP Version:", req.httpVersion);
 
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({
-        data: "Hello World!",
-    }));
-});
 
-server.listen(PORT, () => {
-    console.debug(`Server now up and running, bound to host "${HOST}", listening for connections on port ${PORT}`);
-});
+
+// const server = http.createServer((req, res) => {
+//     console.log("Request – URL:", req.url);
+//     console.log("Request – Method:", req.method);
+//     console.log("Request – Headers:", req.headers);
+//     console.log("Request – HTTP Version:", req.httpVersion);
+
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(JSON.stringify({
+//         data: "Hello World!",
+//     }));
+// });
+
+// server.listen(PORT, () => {
+//     console.debug(`Server now up and running, bound to host "${HOST}", listening for connections on port ${PORT}`);
+// });
