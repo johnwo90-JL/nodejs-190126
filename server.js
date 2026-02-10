c3807fc9698e40b58205c993668bb781aee1d5e6
johnwo90-JL <john-age@jobloop.no>
2026-02-10 10:44:53 +0100

    WIP

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express, { json } from "express";
 import { rootRouter } from "./routes/root.routes";
+import rateLimit from "express-rate-limit";
+import { rateLimitConfig } from "./config/rate-limiter.config";
 
 const app = express();
 
+app.use(rateLimit(rateLimitConfig));
 app.use(json());
 app.use(rootRouter);
 
